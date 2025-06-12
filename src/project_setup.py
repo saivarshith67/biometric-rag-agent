50d722f7f37e213c109ad0030950eb48cfee3f07
saivarshith67 <saivarshith.coder@gmail.com>
2025-06-12 10:40:39 +0000

    minor changes

--- src/project_setup.py
+++ src/project_setup.py
@@ -1,7 +1,11 @@
 from huggingface_hub import login
-from .config import HF_TOKEN
+from .config import HF_TOKEN, DB_URL
 
 def project_setup() -> None:
     if not HF_TOKEN:
         raise ValueError("HUGGINGFACE_ACCESS_TOKEN is not set. Please set it in your environment variables.")
+
+    if not DB_URL:
+        raise ValueError("DB_URL is not set. Please set it in your environment variables.")
+        
     login(token=HF_TOKEN)
